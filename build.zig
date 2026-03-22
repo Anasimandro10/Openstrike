f8f47dc086b8abd4313ddd7cb95543d7d5e34458
Anasimandro9 <anas.lakti123@gmail.com>
2026-03-22 15:17:03 +0100

    fix: librerias Windows necesarias para SDL2 (gdi32, imm32, etc.)

--- build.zig
+++ build.zig
@@ -18,13 +18,18 @@ pub fn build(b: *std.Build) void {
     const is_windows = target.result.os.tag == .windows;
 
     if (is_windows) {
-        // SDL2 MinGW vendored — deps/windows/SDL2/
         exe.addIncludePath(b.path("deps/windows/SDL2/x86_64-w64-mingw32/include"));
         exe.addLibraryPath(b.path("deps/windows/SDL2/x86_64-w64-mingw32/lib"));
         exe.linkSystemLibrary("SDL2");
         exe.linkSystemLibrary("opengl32");
-        exe.linkSystemLibrary("ws2_32"); // ENet — para Sistema 11
-        exe.linkSystemLibrary("winmm");  // ENet — para Sistema 11
+        exe.linkSystemLibrary("ws2_32");   // ENet — sockets UDP
+        exe.linkSystemLibrary("winmm");    // ENet — multimedia timer
+        exe.linkSystemLibrary("gdi32");    // SDL2 — GDI (ventana, fuentes)
+        exe.linkSystemLibrary("setupapi"); // SDL2 — deteccion de joystick/gamepad
+        exe.linkSystemLibrary("ole32");    // SDL2 — COM (audio, input)
+        exe.linkSystemLibrary("oleaut32"); // SDL2 — COM automation
+        exe.linkSystemLibrary("imm32");    // SDL2 — input de texto (IME)
+        exe.linkSystemLibrary("version");  // SDL2 — info de version del SO
     } else {
         exe.linkSystemLibrary("SDL2");
         exe.linkSystemLibrary("GL");
